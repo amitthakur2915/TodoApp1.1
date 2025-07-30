7a7a7752aff33f5d31feab14427fb3d6f9bd9624
amitthakur2915 <amitthakur2915@gmail.com>
2025-07-30 15:46:32 +0530

    Todo App

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+let items = []; // Each item will be { text: "task", done: false }
+
+app.get("/", function (req, res) {
+    res.render("list", { ejes: items }); 
+});
+
+app.post("/", function (req, res) {
+    const newItem = {
+        text: req.body.ele1,
+        done: false
+    };
+    items.push(newItem);
+    res.redirect("/");
+});
+
+// Toggle checkbox status
+app.post("/toggle", function (req, res) {
+    const index = req.body.index;
+    if (items[index]) {
+        items[index].done = !items[index].done;
+    }
+    res.redirect("/");
+});
+
+app.listen(8000, function () {
+    console.log("Server started on port 8000");
+});
