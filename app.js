8866f215e62df6b8f37facc3ddd0566ddb1b72aa
Amit Thakur <amitthakur2915@gmail.com>
2025-08-02 13:31:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-let items = []; 
-
+let items = [];
+let editIndex = -1; 
 app.get("/", function (req, res) {
-    res.render("list", { ejes: items }); 
+    res.render("list", { ejes: items, editIndex: editIndex });
+    editIndex = -1;
 });
 
 app.post("/", function (req, res) {
@@ -20,8 +23,6 @@ app.post("/", function (req, res) {
     items.push(newItem);
     res.redirect("/");
 });
-
-
 app.post("/toggle", function (req, res) {
     const index = req.body.index;
     if (items[index]) {
@@ -29,7 +30,25 @@ app.post("/toggle", function (req, res) {
     }
     res.redirect("/");
 });
-
+app.post("/edit", function (req, res) {
+    editIndex = parseInt(req.body.index);
+    res.redirect("/");
+});
+app.put("/edit/:index", function (req, res) {
+    const index = req.params.index;
+    if (items[index]) {
+        items[index].text = req.body.updatedText;
+    }
+    editIndex = -1;
+    res.redirect("/");
+});
+app.delete("/delete/:index", function (req, res) {
+    const index = req.params.index;
+    if (items[index]) {
+        items.splice(index, 1);
+    }
+    res.redirect("/");
+});
 app.listen(8000, function () {
     console.log("Server started on port 8000");
 });
